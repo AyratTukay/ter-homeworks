b426209d0102ae3ab1033bbe7b6279df15779c81
Ayrat Tukaev <tukay72@yandex.ru>
2026-06-18 21:25:01 +0300

    новая ветка

--- 04/src/providers.tf
+++ 04/src/providers.tf
@@ -5,6 +5,25 @@ terraform {
     }
   }
   required_version = ">1.12.0"
+  backend "s3" {
+    bucket  = "tfstate-tukaev"
+    key     = "dev/terraform.tfstate"
+    region  = "ru-central1"
+         
+    # Встроенный механизм блокировок (Terraform >= 1.6)
+    # Не требует отдельной базы данных!
+    use_lockfile = true
+         
+    endpoints = {
+      s3 = "https://storage.yandexcloud.net"
+    }
+         
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    skip_requesting_account_id  = true
+    skip_s3_checksum            = true
+  }
+
 }
 
 provider "yandex" {
@@ -12,4 +31,5 @@ provider "yandex" {
   cloud_id  = var.cloud_id
   folder_id = var.folder_id
   zone      = var.default_zone
+  service_account_key_file = file("~/.authorized_key.json")
 }
